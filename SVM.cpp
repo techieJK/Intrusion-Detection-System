a3a1cc28a8eb03d7444bc4ffd17a7d07f62f2eb4
techieJK <jagankumar1996@gmail.com>
2017-06-06 11:12:33 +0530

    Updated SVM to Predict data

--- SVM.cpp
+++ SVM.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 int labels[50000];
 float trainingData[50000][7524];
+float testData[8000];
 
 int readData()
 {
@@ -30,11 +31,24 @@ int readData()
 	return j;
 }
 
+void readTestData()
+{
+	fstream DataSet;
+	DataSet.open("test.dat");
+	
+	int i=0,temp1;
+	char temp2;
+	for(i=0;i<=7523;i++)
+	{
+		DataSet>>temp1>>temp2>>testData[i];
+	}
+}
+
 int main()
 {
 	int j=readData(); //Read training data
 	Mat labelsMat(j,1,CV_32SC1,labels);
-	Mat trainingDataMat(j,7523,CV_32FC1,trainingData);
+	Mat trainingDataMat(j,7524,CV_32FC1,trainingData);
    
 	// Set up SVM
 	Ptr<SVM> svm=SVM::create();
@@ -44,13 +58,12 @@ int main()
 	svm->train(trainingDataMat,ROW_SAMPLE,labelsMat);
 	
 
-/*	cout<<"\nEnter test Data:\n ";
-	float testData[8];
-	for(i=0;i<8;i++)
-		cin>>c>>c>>testData[i];
+	cout<<"\nReading test Data:\n ";
+	readTestData();
+	cout<<"\nDONE\n";
 	
-	Mat query = (Mat_<float>(1,8)<<testData[0],testData[1],testData[2],testData[3],testData[4],testData[5],testData[6],testData[7]);
-
+	Mat query(1,7524,CV_32FC1,testData);
+	
 	float res;
 	res=svm->predict(query);
 	
@@ -58,7 +71,7 @@ int main()
 		cout<<"\n"<<res<<"  NEGATIVE class\n";
 	else
 		cout<<"\n"<<res<<"  POSITIVE class\n";
-	*/
+	
 	cout<<"SUCCESS\n";
 	return 0;
 }
